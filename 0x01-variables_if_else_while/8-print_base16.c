a3a6b6847d02100457f7ec71e56e6dfff10217ca
Frankline Chisom <franklinechisom96@gmail.com>
2022-09-10 09:40:43 -0700

    fixed 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers between 0 to 9 and letters between a to f.
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return:0
  */
 int main(void)
 {
-        int i;
+	int i, y;
+	for (i = 0; i < 10; i++)
+	{
+		putchar (i + '0');
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar (y);
+	}
 
-        for (i = 48; i < 58; i++)
-        {
-                putchar(i);
-        }
-        for (i = 97; i < 103; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
+putchar('\n');
+return (0);
 }
